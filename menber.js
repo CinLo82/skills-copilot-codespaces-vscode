19d0d9dd28893c103cbf096a57484bd51ea93de5
Cintia Losada <cinlos3382@gmail.com>
2024-10-18 03:09:18 -0300

    Copilot second commit

---
+++ menber.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        scope: {
+            member: '='
+        },
+        templateUrl: 'app/components/members/member.html',
+        controller: function($scope) {
+            $scope.skills = $scope.member.skills.join(', ');
+        }
+        
+    }
+}
